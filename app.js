593a9dfedc97f7a3f63de57429c94366eec78365
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-04-28 03:16:22 +0100

    added confogs

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000
 
-var clients = {}; 
+var clients = {};
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('server is running');
 });
 
-io.on("connection", function (client) {  
-    client.on("join", function(name){
-    	console.log("Joined: " + name);
-        clients[client.id] = name;
-        client.emit("update", "You have connected to the server.");
-        client.broadcast.emit("update", name + " has joined the server.")
-    });
+io.on("connection", function (client) {
+  client.on("join", function (name) {
+    console.log("Joined: " + name);
+    clients[client.id] = name;
+    client.emit("update", "You have connected to the server.");
+    client.broadcast.emit("update", name + " has joined the server.")
+  });
 
-    client.on("send", function(msg){
-    	console.log("Message: " + msg);
-        client.broadcast.emit("chat", clients[client.id], msg);
-    });
+  client.on("send", function (msg) {
+    console.log("Message: " + msg);
+    client.broadcast.emit("chat", clients[client.id], msg);
+  });
 
-    client.on("disconnect", function(){
-    	console.log("Disconnect");
-        io.emit("update", clients[client.id] + " has left the server.");
-        delete clients[client.id];
-    });
+  client.on("disconnect", function () {
+    console.log("Disconnect");
+    io.emit("update", clients[client.id] + " has left the server.");
+    delete clients[client.id];
+  });
 });
 
 
-http.listen(3000, function(){
+http.listen(port, function () {
   console.log('listening on port 3000');
 });
--- assets/js.js
+++ assets/js.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	var socket = io.connect("http://localhost:3000");
+	var socket = io.connect(window.location.hostname);
 	var ready = false;
 
 	$("#submit").submit(function (e) {
